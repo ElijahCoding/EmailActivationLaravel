e0c214028bff0014f7a4f01c82ed4860a8d08983
LittleElijah <cuiyeqing960904@gmail.com>
2018-06-19 17:14:30 +0300

    preventing sign in

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -35,5 +37,37 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+     protected function validateLogin(Request $request)
+     {
+         $this->validate($request, [
+             $this->username() => [
+                 'required', 'string',
+                 Rule::exists('users')->where(function ($query) {
+                     $query->where('active', true);
+                 })
+             ],
+
+             'password' => 'required|string',
+         ], $this->validationErrors());
+     }
+
+     /**
+     * Get the validation errors for login.
+     *
+     * @return array
+     */
+    protected function validationErrors()
+    {
+        return [
+            $this->username() . '.exists' => 'No account found, or you need to activate your account.'
+        ];
     }
 }
