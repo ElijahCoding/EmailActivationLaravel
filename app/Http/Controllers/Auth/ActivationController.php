e5d473e9d7c60cf869f3c3038c5d48763ffa6b78
LittleElijah <cuiyeqing960904@gmail.com>
2018-06-20 01:25:19 +0300

    activate user

--- app/Http/Controllers/Auth/ActivationController.php
+++ app/Http/Controllers/Auth/ActivationController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Auth;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ActivationController extends Controller
 {
-    public function activate()
+    public function activate(Request $request)
     {
-      
+      $user = User::where('email', $request->email)->where('activation_token', $request->token)->firstOrFail();
+
+      $user->update([
+        'active' => true,
+        'activation_token' => null
+      ]);
+
+      Auth::loginUsingId($user->id);
+
+      return redirect()->route('home');
     }
 }
